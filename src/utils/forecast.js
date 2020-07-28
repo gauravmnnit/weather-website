35170529b1dcbfb2fa1aadff735905710520be06
gauravmnnit <gaurav9519826715@gmail.com>
2020-07-28 19:27:00 +0530

    Forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -18,9 +18,9 @@ const forecast = (latitude, longitude, callback) => {
         undefined,
 
         body.current.weather_descriptions[0] +
-          "It is currently " +
+          ". It is currently " +
           body.current.temperature +
-          "degrees. It feels like " +
+          " degrees. It feels like " +
           body.current.feelslike +
           " degrees. There is " +
           body.current.precip +
