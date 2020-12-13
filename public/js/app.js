fb0008a70d64dc419d120da32260466b5c12fd11
gauravmnnit <68893521+gauravmnnit@users.noreply.github.com>
2020-12-13 21:31:13 +0530

    Add files via upload

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,44 @@
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
-const messageOne = document.querySelector("#message-1");
-const messageTwo = document.querySelector("#message-2");
+const mOne = document.querySelector("#m-1");
+const mTwo = document.querySelector("#m-2");
+const mThree= document.querySelector("#m-3");
+const mFour= document.querySelector("#m-4");
+const mFive= document.querySelector("#m-5");
+const mSix=document.querySelector("#m-6");
+const mSeven=document.querySelector("#m-7");
+const mEight=document.querySelector("#m-8");
+const mNine=document.querySelector("#m-9");
 
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const location = search.value;
 
-  messageOne.textContent = "Loading...";
-  messageTwo.textContent = "";
+  mOne.textContent = "Loading...";
+  mTwo.textContent = "";
+  mThree.textContent="";
+  mFour.textContent="";
+  mFive.textContent="";
+  mSix.textContent="";
+  mSeven.textContent="";
+  mEight.textContent="";
+  mNine.textContent="";
 
   fetch("/weather?address=" + location).then((response) => {
     response.json().then((data) => {
       if (data.error) {
         messageOne.textContent = data.error;
       } else {
-        messageOne.textContent = data.location;
-        messageTwo.textContent = data.forecast;
+        mOne.textContent = "Location : "+data.location;
+        mTwo.textContent = "Observation Time : "+data.observation_time;
+        mThree.textContent= "Temperature : " +data.temperature +" degrees.";
+        mFour.textContent= "Weather : " +data.weather;
+        mFive.textContent="Feels Like : "+data.feels_like + " degrees.";
+        mSix.textContent="Precipitation : "+data.precipitation ;
+        mSeven.textContent="Humidity : "+data.humidity+" %";
+        mEight.textContent="Wind Speed : "+data.wind_speed+ " km/h";
+        mNine.textContent="UV index : " +data.uv_index;
       }
     });
   });
